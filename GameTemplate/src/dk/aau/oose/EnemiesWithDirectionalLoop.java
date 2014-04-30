391202451a9f022c688ff9efbb2985f99999a696
pashkadkk <pgrosi11@student.aau.dk>
2014-04-30 12:05:59 +0200

    EnemiesWithDirectionalLoop java file

---
+++ GameTemplate/src/dk/aau/oose/EnemiesWithDirectionalLoop.java
@@ -0,0 +1,13 @@
+package dk.aau.oose;
+
+import dk.aau.oose.Zombie.Direction;
+
+public interface EnemiesWithDirectionalLoop {
+
+	public void directionFunction (float targetX, float targetY);
+
+	public Direction directions ();
+	public void directionLoop(Direction direction);
+	
+	
+}
